c5f0048cd7c89b99161525699dcaadbae23876ee
gabb <bie.ritan112@gmail.com>
2023-02-22 08:08:32 +0700

    menambahkan header

---
+++ app/src/main/java/com/example/androidrawlabspace/rawlabspace/profil/TestingClass.kt
@@ -0,0 +1,4 @@
+package com.example.androidrawlabspace.rawlabspace.profil
+
+class TestingClass {
+}
---
+++ app/src/main/java/com/example/androidrawlabspace/rawlabspace/shared/ui/HeaderView.kt
@@ -0,0 +1,12 @@
+package com.example.androidrawlabspace.rawlabspace.shared.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidrawlabspace.R
+
+class HeaderView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_header)
+    }
+}
